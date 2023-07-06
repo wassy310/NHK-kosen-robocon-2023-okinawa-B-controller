df666af994624cf06face48a597c2de31b2b2d56
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2023-07-07 00:17:16 +0900

    Setting up to use Bluetooth

--- android/app/src/main/kotlin/com/example/robo_controller/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/robo_controller/MainActivity.kt
@@ -1,6 +1,34 @@
 package com.example.robo_controller
 
 import io.flutter.embedding.android.FlutterActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity: FlutterActivity() {
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val permission = Manifest.permission.ACCESS_FINE_LOCATION
+            val permissionCheck = ContextCompat.checkSelfPermission(this, permission)
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(permission), 1001)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1001) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // パーミッションが許可
+            } else {
+                // パーミッションが拒否
+            }
+        }
+    }
 }
