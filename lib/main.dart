18b2cfd13a72724249239ddebc138803f95c3dfd
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2023-07-07 10:09:58 +0900

    Add new func(check connection status)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'UI/button/emergency_stop.dart';
+import 'UI/status/connection.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Robot controller/to check status',
       home: MyHomePage(),
     );
   }
@@ -39,6 +40,7 @@ class MyHomePage extends StatelessWidget {
               debugPrint('pushed');
             },
           ),
+          const ConnectionStatusWidget(),
         ],
       ),
     );
