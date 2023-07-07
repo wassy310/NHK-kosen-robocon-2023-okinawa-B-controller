bacb1168961ba66a98c719fa55c229c3ee6ebd2d
Wassy | Ryoga Washizawa <74349349+wassy310@users.noreply.github.com>
2023-07-07 09:55:49 +0900

    Add new func(check connection status)

--- lib/UI/status/connection.dart
+++ lib/UI/status/connection.dart
@@ -1,40 +1,81 @@
+import 'package:flutter/material.dart';
+
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-void main() {
-  scanAndConnectToDevice();
+class ConnectionStatusWidget extends StatefulWidget {
+  const ConnectionStatusWidget({super.key});
+
+  @override
+  ConnectionStatusWidgetState createState() => ConnectionStatusWidgetState();
 }
 
-void scanAndConnectToDevice() async {
-  FlutterBluePlus flutterBluePlus = FlutterBluePlus.instance;
+class ConnectionStatusWidgetState extends State<ConnectionStatusWidget> {
+  BluetoothDevice? device;
+  bool connected = false;
 
-  List<ScanResult> scanResults =
-      await flutterBluePlus.scan(timeout: const Duration(seconds: 4)).toList();
+  @override
+  void initState() {
+    super.initState();
+    scanAndConnectToDevice();
+  }
 
-  for (var result in scanResults) {
-    if (result.device.name == 'ESP32') {
-      await result.device.connect(autoConnect: false);
+  @override
+  void dispose() {
+    device?.disconnect();
+    super.dispose();
+  }
 
-      discoverServices(result.device);
-      break;
+  void scanAndConnectToDevice() async {
+    FlutterBluePlus flutterBluePlus = FlutterBluePlus.instance;
+
+    List<ScanResult> scanResults = await flutterBluePlus
+        .scan(timeout: const Duration(seconds: 4))
+        .toList();
+
+    for (var result in scanResults) {
+      if (result.device.name == 'ESP32') {
+        await result.device.connect(autoConnect: false);
+        setState(() {
+          device = result.device;
+        });
+        discoverServices(result.device);
+        break;
+      }
     }
   }
-}
 
-void discoverServices(BluetoothDevice device) async {
-  List<BluetoothService> services = await device.discoverServices();
+  void discoverServices(BluetoothDevice device) async {
+    List<BluetoothService> services = await device.discoverServices();
 
-  for (var service in services) {
-    if (service.uuid == Guid('service UUID')) {
-      List<BluetoothCharacteristic> characteristics = service.characteristics;
-      for (var characteristic in characteristics) {
-        if (characteristic.uuid == Guid('characteristic UUID')) {
-          List<int> value = await characteristic.read();
-          print('read: $value');
+    for (var service in services) {
+      if (service.uuid == Guid('service UUID')) {
+        List<BluetoothCharacteristic> characteristics = service.characteristics;
+        for (var characteristic in characteristics) {
+          if (characteristic.uuid == Guid('characteristic UUID')) {
+            List<int> value = await characteristic.read();
+            debugPrint('read: $value');
 
-          await characteristic.write([0x01, 0x02, 0x03]);
-          print('write complete');
+            await characteristic.write([0x01, 0x02, 0x03]);
+            debugPrint('write complete');
+
+            setState(() {
+              connected = true;
+            });
+          }
         }
       }
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 40,
+      right: 70,
+      child: Text(
+        connected ? 'Connected' : 'Not Connected',
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
